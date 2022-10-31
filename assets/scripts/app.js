d664f5843786e18e2a9791db611655cb795383ea
GiannisDarze <reb@hotmail.gr>
2022-10-31 16:03:50 +0200

    Added more DOM manipulation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,6 +9,7 @@ class DOMHelper {
     const element = document.getElementById(elementId);
     const destination = document.querySelector(newDestination);
     destination.append(element);
+    element.scrollIntoView({ behavior: "smooth" });
   }
 }
 
@@ -36,9 +37,10 @@ class GrandMaster {
   }
 }
 class InfoWindow extends GrandMaster {
-  constructor(closeNotifier) {
-    super();
+  constructor(closeNotifier, text, hostElementId) {
+    super(hostElementId);
     this.closeNotifierHandler = closeNotifier;
+    this.text = text;
     this.render();
   }
 
@@ -50,7 +52,26 @@ class InfoWindow extends GrandMaster {
   render() {
     const newInfoElement = document.createElement("div");
     newInfoElement.className = "card";
-    newInfoElement.textContent = "DUMMY!";
+    const infoWindowTemplate = document.getElementById("info-window");
+    const infoWindowBody = document.importNode(
+      infoWindowTemplate.content,
+      true
+    );
+    infoWindowBody.querySelector("p").textContent = this.text;
+    newInfoElement.append(infoWindowBody);
+
+    const hostElPosLeft = this.hostElement.offsetLeft;
+    const hostElPosTop = this.hostElement.offsetTop;
+    const hostElHeight = this.hostElement.clientHeight;
+    const parentElementScroll = this.hostElement.parentElement.scrollTop;
+
+    const x = hostElPosLeft + 20;
+    const y = hostElPosTop + hostElHeight - parentElementScroll - 10;
+
+    newInfoElement.style.position = "absolute";
+    newInfoElement.style.left = x + "px";
+    newInfoElement.style.top = y + "px";
+
     newInfoElement.addEventListener("click", this.closeInfoWindow);
     this.element = newInfoElement;
   }
@@ -70,9 +91,16 @@ class ProjectItem {
     if (this.hasActiveInfoBox) {
       return;
     }
-    const infoWindow = new InfoWindow(() => {
-      this.hasActiveInfoBox = false;
-    });
+    const projectElement = document.getElementById(this.id);
+    const infoWindowText = projectElement.dataset.extraInfo;
+
+    const infoWindow = new InfoWindow(
+      () => {
+        this.hasActiveInfoBox = false;
+      },
+      infoWindowText,
+      this.id
+    );
     infoWindow.attach();
     this.hasActiveInfoBox = true;
   }
@@ -80,7 +108,7 @@ class ProjectItem {
   moreInfoButton() {
     const itemElement = document.getElementById(this.id);
     const moreInfoBtn = itemElement.querySelector("button:first-of-type");
-    moreInfoBtn.addEventListener("click", this.showMoreInfoHandler);
+    moreInfoBtn.addEventListener("click", this.showMoreInfoHandler.bind(this));
   }
 
   switchButton(type) {
